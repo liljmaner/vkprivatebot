ca104010f5740bb05d8ccd9b2d3147d1f31c55cd
liljmaner <minecraftrposeven@gmail.com>
2025-04-27 00:20:43 +0300

    123

--- clasess/request_check.js
+++ clasess/request_check.js
@@ -7,6 +7,7 @@ class request_check
   Check = (check_object , object_to_check,callback) => 
   {
    const valid_obj = this.validator.validate(object_to_check,check_object)//object_to_check,check_object
+   console.log(object_to_check);
    console.log(valid_obj.valid);
    if (valid_obj.valid == true)
      return callback("sucess")
